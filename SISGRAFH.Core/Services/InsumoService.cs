bf9f62f3a113fba66e240a68252f2e199a8b98ef
MirkoRZ <mirkorojas345@gmail.com>
2022-10-16 02:25:37 -0500

    Controller isnumos completado

--- SISGRAFH.Core/Services/InsumoService.cs
+++ SISGRAFH.Core/Services/InsumoService.cs
@@ -35,22 +35,59 @@ namespace SISGRAFH.Core.Services
 
         public async Task<beInsumo> UpdateInsumo(beInsumo insumo)
         {
-            var userDb = await _unitOfWork.Insumo.GetByIdAsync(insumo.Id);
-            if (userDb == null)
+            var insumoDb = await _unitOfWork.Insumo.GetByIdAsync(insumo.Id);
+            if (insumoDb == null)
             {
                 return await PostInsumo(insumo);
             };
-            userDb.nombre = insumo.nombre;
-            userDb.descripcion = insumo.descripcion;
-            userDb.unidad = insumo.unidad;
-            userDb.unidad_insumo = insumo.unidad_insumo;
-            userDb.cantidad_insumo = insumo.cantidad_insumo;
-            userDb.categoria = insumo.categoria;
-            userDb.marca = insumo.marca;
-            userDb.costo_unitario = insumo.costo_unitario;
-            userDb.stock_disponible = insumo.stock_disponible;
-
-            return await _unitOfWork.Insumo.UpdateOneAsync(insumo);
+            insumoDb.nombre = insumo.nombre;
+            insumoDb.descripcion = insumo.descripcion;
+            insumoDb.unidad = insumo.unidad;
+            insumoDb.unidad_insumo = insumo.unidad_insumo;
+            insumoDb.cantidad_insumo = insumo.cantidad_insumo;
+            insumoDb.categoria = insumo.categoria;
+            insumoDb.marca = insumo.marca;
+            insumoDb.costo_unitario = insumo.costo_unitario;
+            insumoDb.stock_disponible = insumo.stock_disponible;
+            switch (insumoDb.GetType().Name)
+            {
+                case "beEspiral": 
+                    var esp=(beEspiral)insumoDb;
+                    var espiralPut=(beEspiral)insumo;
+                    esp.diametro = espiralPut.diametro;
+                    esp.longitud = espiralPut.longitud;
+                    esp.grosor_union = espiralPut.grosor_union;
+                    esp.numero_agujeros = espiralPut.numero_agujeros;
+                    esp.material = espiralPut.material;
+                    break;
+                case "beTinta":
+                    var tinta = (beTinta)insumoDb;
+                    var tintaPut = (beTinta)insumo;
+                    tinta.compatibilidad = tintaPut.compatibilidad;
+                    tinta.color = tintaPut.color;
+                    tinta.rendimiento = tintaPut.rendimiento;
+                    tinta.tecnologia_impresion = tintaPut.tecnologia_impresion;
+                    break;
+                case "bePapel":
+                    var papel = (bePapel)insumoDb;
+                    var papelPut = (bePapel)insumo;
+                    papel.ancho = papelPut.ancho;
+                    papel.largo = papelPut.largo;
+                    papel.gramaje = papelPut.gramaje;
+                    papel.tipo = papelPut.tipo;
+                    papel.color = papelPut.color;
+                    break;
+                case "bePelicula_adhesiva":
+                    var pelicula = (bePelicula_adhesiva)insumoDb;
+                    var peliculaPut = (bePelicula_adhesiva)insumo;
+                    pelicula.ancho = peliculaPut.ancho;
+                    pelicula.grosor = peliculaPut.grosor;
+                    pelicula.capa_adhesivo = peliculaPut.capa_adhesivo;
+                    pelicula.capa_superficie = peliculaPut.capa_superficie;
+                    pelicula.acabado = peliculaPut.acabado;
+                    break;
+            }
+            return await _unitOfWork.Insumo.UpdateOneAsync(insumoDb);
         }
     }
 }
