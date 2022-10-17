7db314f5cfe5fad5ab8b3d964d101193a2739c52
Diego Cedron <70548640+DiegoCedron7@users.noreply.github.com>
2022-10-16 23:49:33 -0500

    Creacion clienteDto y Mapeado de clienteDto

---
+++ SISGRAFH.Core/DTOs/Cliente/ClienteDto.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISGRAFH.Core.DTOs.Cliente
+{
+    public class ClienteDto
+    {
+        public string Id { get; set; }
+        public string IdUsuario { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string ApellidoPaterno { get; set; }
+
+        public string ApellidoMaterno { get; set; }
+
+        public string Correo { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string IdHistorialPedidos { get; set; }
+    }
+}
