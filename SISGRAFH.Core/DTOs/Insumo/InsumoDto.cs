1a2170abe421ce7623624a185e1620321351183e
Diegocalderon31 <diegocalderonivo@gmail.com>
2022-11-15 19:34:02 -0500

    Cambio "id"

--- SISGRAFH.Core/DTOs/Insumo/InsumoDto.cs
+++ SISGRAFH.Core/DTOs/Insumo/InsumoDto.cs
@@ -8,7 +8,7 @@ namespace SISGRAFH.Core.DTOs.Insumo
 {
     public class InsumoDto
     {
-        public string Id { get; set; }
+        public string id { get; set; }
         public string nombre { get; set; }
         public string descripcion { get; set; }
         public string unidad { get; set; }
