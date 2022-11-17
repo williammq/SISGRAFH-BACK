e4d69c121e91e766e02177e5384dad144da0d50e
Renzo Guerra Candela <201810589@urp.edu.pe>
2022-11-17 15:16:19 -0500

    Movimientos

---
+++ SISGRAFH.Core/Interfaces/IUbigeoRepository.cs
@@ -0,0 +1,15 @@
+﻿using SISGRAFH.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISGRAFH.Core.Interfaces
+{
+    public interface IUbigeoRepository : IRepository<beUbigeo>
+    {
+        Task<List<beUbigeo>> GetAllUbigeo();
+        Task<beUbigeo> GetByIdUbigeo(string id);
+    }
+}
---
+++ SISGRAFH.Core/Interfaces/IUbigeoService.cs
@@ -0,0 +1,15 @@
+﻿using SISGRAFH.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISGRAFH.Core.Interfaces
+{
+    public interface IUbigeoService
+    {
+        Task<List<beUbigeo>> GetAllUbigeo();
+        Task<beUbigeo> GetByIdUbigeo(string id);
+    }
+}
